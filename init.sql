3ecafd6949f7ce1ba51d763f88c36ea9e28b9986
Sahil Kumar <itssahilhere31@gmail.com>
2025-09-05 19:39:23 +0530

    removed backend files

--- init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE auth_system;
-
-USE auth_system;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL
-);
